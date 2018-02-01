08ab0588ddcb09c936bac0542835054e3c9bafdf
sumehta <smehta921@gmail.com>
2018-02-01 18:01:58 -0500

    bidir lstm initial commit

--- model.py
+++ model.py
@@ -3,3 +3,169 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 
+
+class BiLSTM(nn.Module):
+    def __init__(self, emsize, nhidden, ntokens, nlayers, noise_radius=0.2,
+                 hidden_init=False, dropout=0, gpu=False):
+        super(BiLSTM, self).__init__()
+        self.nhidden = nhidden
+        self.emsize = emsize
+        self.ntokens = ntokens
+        self.nlayers = nlayers
+        self.noise_radius = noise_radius
+        self.hidden_init = hidden_init
+        self.dropout = dropout
+        self.gpu = gpu
+
+        self.start_symbols = to_gpu(gpu, Variable(torch.ones(10, 1).long()))
+
+        # Vocabulary embedding
+        self.embedding = nn.Embedding(ntokens, emsize)
+
+        # RNN Encoder and Decoder
+        self.lstm = nn.LSTM(input_size=emsize,
+                               hidden_size=nhidden,
+                               num_layers=nlayers,
+                               dropout=dropout,
+                               batch_first=True,
+                               bidirectional=True)
+        self.init_weights()
+
+    def init_weights(self):
+        initrange = 0.1
+
+        # Initialize Vocabulary Matrix Weight
+        self.embedding.weight.data.uniform_(-initrange, initrange)
+
+        # Initialize Encoder and Decoder Weights
+        for p in self.lstm.parameters():
+            p.data.uniform_(-initrange, initrange)
+
+    def init_hidden(self, bsz):
+        zeros1 = Variable(torch.zeros(self.nlayers, bsz, self.nhidden))
+        zeros2 = Variable(torch.zeros(self.nlayers, bsz, self.nhidden))
+        return (to_gpu(self.gpu, zeros1), to_gpu(self.gpu, zeros2))
+
+    def init_state(self, bsz):
+        zeros = Variable(torch.zeros(self.nlayers, bsz, self.nhidden))
+        return to_gpu(self.gpu, zeros)
+
+    def store_grad_norm(self, grad):
+        norm = torch.norm(grad, 2, 1)
+        self.grad_norm = norm.detach().data.mean()
+        return grad
+
+    def forward(self, indices, lengths, noise, encode_only=False):
+        batch_size, maxlen = indices.size()
+
+        hidden = self.encode(indices, lengths, noise)
+
+        if encode_only:
+            return hidden
+
+        if hidden.requires_grad:
+            hidden.register_hook(self.store_grad_norm)
+
+        decoded = self.decode(hidden, batch_size, maxlen,
+                              indices=indices, lengths=lengths)
+
+        return decoded
+
+    def encode(self, indices, lengths, noise):
+        embeddings = self.embedding(indices)
+        packed_embeddings = pack_padded_sequence(input=embeddings,
+                                                 lengths=lengths,
+                                                 batch_first=True)
+
+        # Encode
+        packed_output, state = self.encoder(packed_embeddings)
+
+        hidden, cell = state
+        # batch_size x nhidden
+        hidden = hidden[-1]  # get hidden state of last layer of encoder
+
+        # normalize to unit ball (l2 norm of 1) - p=2, dim=1
+        norms = torch.norm(hidden, 2, 1)
+
+        # For older versions of PyTorch use:
+        #hidden = torch.div(hidden, norms.expand_as(hidden))
+        # For newest version of PyTorch (as of 8/25) use this:
+        hidden = torch.div(hidden, norms.unsqueeze(1).expand_as(hidden))
+
+        if noise and self.noise_radius > 0:
+            gauss_noise = torch.normal(means=torch.zeros(hidden.size()),
+                                       std=self.noise_radius)
+            hidden = hidden + to_gpu(self.gpu, Variable(gauss_noise))
+
+        return hidden
+
+    def decode(self, hidden, batch_size, maxlen, indices=None, lengths=None):
+        # batch x hidden
+        all_hidden = hidden.unsqueeze(1).repeat(1, maxlen, 1)
+
+        if self.hidden_init:
+            # initialize decoder hidden state to encoder output
+            state = (hidden.unsqueeze(0), self.init_state(batch_size))
+        else:
+            state = self.init_hidden(batch_size)
+
+        embeddings = self.embedding_decoder(indices)
+        augmented_embeddings = torch.cat([embeddings, all_hidden], 2)
+        packed_embeddings = pack_padded_sequence(input=augmented_embeddings,
+                                                 lengths=lengths,
+                                                 batch_first=True)
+
+        packed_output, state = self.decoder(packed_embeddings, state)
+        output, lengths = pad_packed_sequence(packed_output, batch_first=True)
+
+        # reshape to batch_size*maxlen x nhidden before linear over vocab
+        decoded = self.linear(output.contiguous().view(-1, self.nhidden))
+        decoded = decoded.view(batch_size, maxlen, self.ntokens)
+
+        return decoded
+
+    def generate(self, hidden, maxlen, sample=True, temp=1.0):
+        """Generate through decoder; no backprop"""
+
+        batch_size = hidden.size(0)
+
+        if self.hidden_init:
+            # initialize decoder hidden state to encoder output
+            state = (hidden.unsqueeze(0), self.init_state(batch_size))
+        else:
+            state = self.init_hidden(batch_size)
+
+        # <sos>
+        self.start_symbols.data.resize_(batch_size, 1)
+        self.start_symbols.data.fill_(1)
+
+        embedding = self.embedding_decoder(self.start_symbols)
+        inputs = torch.cat([embedding, hidden.unsqueeze(1)], 2)
+
+        # unroll
+        all_indices = []
+        # print (maxlen)
+        for i in range(maxlen):
+            output, state = self.decoder(inputs, state)
+            overvocab = self.linear(output.squeeze(1))
+
+            if not sample:
+                vals, indices = torch.max(overvocab, 1)
+            else:
+                # sampling
+                probs = F.softmax(overvocab/temp)
+                indices = torch.multinomial(probs, 1)
+
+            # print(len(indices))
+            if indices.dim() == 1:
+                indices = indices.unsqueeze(1)
+
+            all_indices.append(indices)
+
+            # print (indices.size())
+            embedding = self.embedding_decoder(indices)
+            inputs = torch.cat([embedding, hidden.unsqueeze(1)], 2)
+
+        max_indices = torch.cat(all_indices, 1)
+
+        return max_indices
